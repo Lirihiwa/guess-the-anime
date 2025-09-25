28087e4da4692185bcda20bda6f7fbe57bcb3e68
Lirihiwa <144426054+Lirihiwa@users.noreply.github.com>
2025-09-25 19:40:27 +0300

    Add timer

---
+++ Services/Timer/ITimerService.cs
@@ -0,0 +1,10 @@
+﻿namespace GuessTheAnime.Services.Timer
+{
+    public interface ITimerService
+    {
+        void Start(int timeout);
+
+        event Action TimesUp;
+        event Action<int> TimeIsChanged;
+    }
+}
---
+++ Services/Timer/TimerService.cs
@@ -0,0 +1,53 @@
+﻿using System.Timers;
+
+namespace GuessTheAnime.Services.Timer
+{
+    public class TimerService : ITimerService
+    {
+        private System.Timers.Timer _timer;
+        private int _timeout;
+        private const int _interval = 1000;
+
+        public event Action? TimesUp;
+        public event Action<int>? TimeIsChanged;
+
+        public TimerService()
+        {
+            _timer = new System.Timers.Timer();
+            _timer.Interval = _interval;
+            _timer.AutoReset = true;
+
+            _timer.Elapsed += OnTimerElapsed;
+        }
+
+        public void Start(int timeout)
+        {
+            Timeout = timeout;
+            
+            _timer.Start();
+        }
+
+        private void OnTimerElapsed(object? sender, ElapsedEventArgs e)
+        {
+            Timeout = _timeout - 1;
+        }
+
+        private int Timeout
+        {
+            get { return  _timeout; }
+            set
+            {
+                if (value == 0)
+                {
+                    _timer.Stop();
+                    TimeIsChanged?.Invoke(value);
+                    TimesUp?.Invoke();
+                    return;
+                }
+
+                _timeout = value;
+                TimeIsChanged?.Invoke(value);
+            }
+        }
+    }
+}
