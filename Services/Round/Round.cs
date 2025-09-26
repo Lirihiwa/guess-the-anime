40377ced48f03f402ffdcca4a0aa822714e5405a
Lirihiwa <144426054+Lirihiwa@users.noreply.github.com>
2025-09-26 22:53:00 +0300

    Create IRoundService.cs and Round.cs

---
+++ Services/Round/Round.cs
@@ -0,0 +1,9 @@
+﻿namespace GuessTheAnime.Services.Round
+{
+    public class Round
+    {
+        private int _ordinalNumber;
+        private int _correctAnswerIndex;
+        private string[]? _answers;
+    }   
+}
